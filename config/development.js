e0eeb7e4997221622cf141088f3acbab95096eec
Paari Gopal <paari@lob.com>
2019-03-28 13:35:46 -0700

    fix(db): removes merge strings from config files

--- config/development.js
+++ config/development.js
@@ -1,18 +1,10 @@
 'use strict';
 
 module.exports = {
-<<<<<<< HEAD
-=======
-  PORT: 3000,
->>>>>>> f8fee37... chore(db): sets up initial boilerplate for postgres database
   DB_HOST: 'localhost',
   DB_NAME: 'sfmovies_development',
   DB_PASSWORD: '',
   DB_PORT: 5432,
-<<<<<<< HEAD
   DB_USER: 'sfmovies_user',
   PORT: 3000
-=======
-  DB_USER: 'sfmovies_user'
->>>>>>> f8fee37... chore(db): sets up initial boilerplate for postgres database
 };
