a9bf1db1cf0494ab3a2a2b2665aab83eb4f4278b
Paari Gopal <paari@lob.com>
2019-03-29 15:49:31 -0700

    test(api): integration tests and formatting fixes

--- test/plugins/features/movies/controller.test.js
+++ test/plugins/features/movies/controller.test.js
@@ -3,12 +3,12 @@
 const Controller = require('../../../../lib/plugins/features/movies/controller');
 
 describe('movie controller', () => {
-  
+
   describe('create', () => {
-    
+
     it('creates a movie', async () => {
       const payload = { title: 'Forrest Gump' };
-      
+
       const movie = await Controller.create(payload);
 
       expect(movie.get('title')).to.eql(payload.title);
