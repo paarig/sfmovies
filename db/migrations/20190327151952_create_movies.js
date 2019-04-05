ec879ae172ea07905da39044f1c676ea3ee4ba26
Paari Gopal <paari@lob.com>
2019-04-04 17:53:11 -0700

    feat(movies): associate locations with movies

--- db/migrations/20190327151952_create_movies.js
+++ db/migrations/20190327151952_create_movies.js
@@ -5,9 +5,17 @@ exports.up = async (Knex) => {
     table.increments('id').primary();
     table.text('title').notNullable();
     table.integer('release_year');
+  }).createTable('locations', (table) => {
+    table.increments('id').primary();
+    table.string('name');
+  }).createTable('locations_movies', (table) => {
+    table.integer('movie_id').unsigned().references('movies.id');
+    table.integer('location_id').unsigned().references('locations.id');
   });
 };
 
 exports.down = async (Knex) =>  {
-  await Knex.schema.dropTable('movies');
+  await Knex.schema.dropTable('movies')
+  .dropTable('locations')
+  .dropTable('locations_movies');
 };
--- db/seeds/index.js
+++ db/seeds/index.js
@@ -2,7 +2,15 @@
 
 const Movies = require('./data/movies');
 
+const Locations = require('./data/locations');
+
 exports.seed = async (Knex) => {
-  await Knex('movies').truncate();
+  await Knex.raw('TRUNCATE TABLE movies, locations, locations_movies CASCADE');
   await Knex('movies').insert(Movies);
+  await Knex('locations').insert(Locations);
+  const locationIds = await Knex('locations').select('id');
+  for (let i = 0; i < locationIds.length; i++) {
+    const movies = await Knex('movies').orderByRaw('RANDOM()').limit(1);
+    await Knex('locations_movies').insert({ movie_id: movies[0].id, location_id: locationIds[i].id });
+  }
 };
