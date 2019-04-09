7e6249aae28e1367bb61208045a5b4e1b19bb8c5
Paari Gopal <paari@lob.com>
2019-04-08 17:55:36 -0700

    fix(test): migrated controller tests to name

--- test/plugins/features/movies/controller.test.js
+++ test/plugins/features/movies/controller.test.js
@@ -9,12 +9,12 @@ const Knex = require('../../../../lib/libraries/knex');
 describe('movie controller', () => {
 
   const firstMovie = {
-    title: 'test movie 1',
+    name: 'test movie 1',
     release_year: 1985
   };
 
   const secondMovie = {
-    title: 'test movie 2',
+    name: 'test movie 2',
     release_year: 1990
   };
 
@@ -31,17 +31,17 @@ describe('movie controller', () => {
 
       const movie = await Controller.createMovie(payload);
 
-      expect(movie.get('title')).to.eql(payload.title);
+      expect(movie.get('name')).to.eql(payload.name);
     });
 
     it('creates a new location', async () => {
       const payload = { name: 'test location' };
 
-      const movieId = await new Movie({ title: firstMovie.title }).fetch().get('id');
+      const movieId = await new Movie({ name: firstMovie.name }).fetch().get('id');
 
       const movie = await Controller.createLocation(payload, movieId);
 
-      expect(movie.get('title')).to.eql(firstMovie.title);
+      expect(movie.get('name')).to.eql(firstMovie.name);
       expect(movie.related('locations').length).to.eql(1);
       expect(movie.related('locations').at(0).get('name')).to.eql('test location');
     });
@@ -49,12 +49,12 @@ describe('movie controller', () => {
     it('attaches old location to new movie', async () => {
       const payload = { name: 'test location' };
 
-      const firstId = await new Movie({ title: firstMovie.title }).fetch().get('id');
+      const firstId = await new Movie({ name: firstMovie.name }).fetch().get('id');
       await Controller.createLocation(payload, firstId);
-      const secondId = await new Movie({ title: secondMovie.title }).fetch().get('id');
+      const secondId = await new Movie({ name: secondMovie.name }).fetch().get('id');
       const movie = await Controller.createLocation(payload, secondId);
 
-      expect(movie.get('title')).to.eql(secondMovie.title);
+      expect(movie.get('name')).to.eql(secondMovie.name);
       expect(movie.related('locations').length).to.eql(1);
       expect(movie.related('locations').at(0).get('name')).to.eql('test location');
     });
@@ -66,15 +66,15 @@ describe('movie controller', () => {
     it('finds a movie w/o params', async () => {
       const movies = await Controller.find();
       expect(movies.length).to.eql(2);
-      expect(movies.at(0).get('title')).to.eql(firstMovie.title);
+      expect(movies.at(0).get('name')).to.eql(firstMovie.name);
       expect(movies.at(0).get('release_year')).to.eql(firstMovie.release_year);
-      expect(movies.at(1).get('title')).to.eql(secondMovie.title);
+      expect(movies.at(1).get('name')).to.eql(secondMovie.name);
       expect(movies.at(1).get('release_year')).to.eql(secondMovie.release_year);
     });
 
     it('finds a movie with order param', async () => {
       const params = {
-        title: secondMovie.title,
+        title: secondMovie.name,
         year: secondMovie.release_year,
         start_yr: firstMovie.release_year,
         end_yr: 1995,
@@ -83,30 +83,30 @@ describe('movie controller', () => {
 
       const movies = await Controller.find(params);
       expect(movies.length).to.eql(1);
-      expect(movies.at(0).get('title')).to.eql(secondMovie.title);
+      expect(movies.at(0).get('name')).to.eql(secondMovie.name);
       expect(movies.at(0).get('release_year')).to.eql(secondMovie.release_year);
     });
 
     it('finds a movie with order & order_by params', async () => {
       const params = {
-        title: secondMovie.title,
+        title: secondMovie.name,
         year: secondMovie.release_year,
         start_yr: firstMovie.release_year,
         end_yr: 1995,
         order: 'desc',
-        order_by: 'title'
+        order_by: 'name'
       };
 
       const movies = await Controller.find(params);
 
       expect(movies.length).to.eql(1);
-      expect(movies.at(0).get('title')).to.eql(secondMovie.title);
+      expect(movies.at(0).get('name')).to.eql(secondMovie.name);
       expect(movies.at(0).get('release_year')).to.eql(secondMovie.release_year);
     });
 
     it('finds a movie with order_by param', async () => {
       const params = {
-        title: secondMovie.title,
+        title: secondMovie.name,
         year: secondMovie.release_year,
         start_yr: firstMovie.release_year,
         end_yr: 1995,
@@ -116,7 +116,7 @@ describe('movie controller', () => {
       const movies = await Controller.find(params);
 
       expect(movies.length).to.eql(1);
-      expect(movies.at(0).get('title')).to.eql(secondMovie.title);
+      expect(movies.at(0).get('name')).to.eql(secondMovie.name);
       expect(movies.at(0).get('release_year')).to.eql(secondMovie.release_year);
     });
 
